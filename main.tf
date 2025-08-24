f2e96e8b37cd41e211b93c7d75dd56a29c3b4bd8
eswar-sudo <eswarkumar501@gmail.com>
2025-08-24 12:15:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,25 @@ provider "aws" {
   region = var.region
 }
 
+# Step 1: Generate random password
+resource "random_password" "rds_password" {
+  length  = 16
+  special = true
+}
+
+# Step 2: Create Secrets Manager secret
+resource "aws_secretsmanager_secret" "rds_secret" {
+  name = "${var.name}-credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "rds_secret_version" {
+  secret_id     = aws_secretsmanager_secret.rds_secret.id
+  secret_string = jsonencode({
+    username = var.username
+    password = random_password.rds_password.result
+  })
+}
+
 resource "aws_db_subnet_group" "this" {
   name       = "${var.db_identifier}-subnet-group"
   subnet_ids = var.subnet_ids
@@ -34,7 +53,7 @@ resource "aws_rds_cluster" "aurora" {
   engine                 = "aurora-mysql"
   engine_version         = var.engine_version
   master_username        = var.db_username
-  master_password        = var.db_password
+  masyer_password = random_password.rds_password.result
   database_name          = var.db_name
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.this.id]
@@ -58,7 +77,7 @@ resource "aws_db_instance" "mysql" {
   instance_class         = var.instance_class
   allocated_storage      = var.storage_gb
   username               = var.db_username
-  password               = var.db_password
+  password               = random_password.rds_password.result
   db_name                = var.db_name
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.this.id]
@@ -73,7 +92,7 @@ resource "aws_rds_cluster" "mysql_cluster" {
   engine                 = "mysql"
   engine_version         = var.engine_version
   master_username        = var.db_username
-  master_password        = var.db_password
+  masyer_password        = random_password.rds_password.result
   database_name          = var.db_name
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.this.id]
