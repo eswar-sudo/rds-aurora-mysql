1f1130de2d2cba1100e594a60856408065785891
eswar-sudo <eswarkumar501@gmail.com>
2025-08-25 12:45:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,14 +35,14 @@ resource "aws_security_group" "this" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 }
 
