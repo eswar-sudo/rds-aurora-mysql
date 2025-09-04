3c5fc0a6427104c49a7f6bcdaf18249a047b99c1
eswar-sudo <eswarkumar501@gmail.com>
2025-09-04 12:09:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,10 +29,10 @@ variable "db_name" {
   type        = string
 }
 
-#variable "db_username" {
- # description = "Master username"
- # type        = string
-#}
+variable "db_username" {
+  description = "Master username"
+  type        = string
+}
 
 #variable "db_password" {
  # description = "Master password"
